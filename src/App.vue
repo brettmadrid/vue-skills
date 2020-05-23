2894245f250a089b006f0635b156938c7fa1a984
brettmadrid <brettmadrid9@gmail.com>
2020-05-22 21:55:09 -0700

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Can pass in props to component ala React -->
+    <Skills />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Skills from "./components/Skills.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
+  // components used in app
   components: {
-    HelloWorld
+    Skills
   }
-}
+};
 </script>
 
+// in app.vue, global styles can be put here
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
+body {
+  background-color: #eeeeee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 50%;
 }
 </style>
